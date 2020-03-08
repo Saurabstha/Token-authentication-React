0a8b97818b9a174323c9bee706f1c69a7821b492
unknown <saurab.shrestha99@gmail.com>
2020-03-08 12:21:37 +0545

    token authentication localstorage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import { Link, Switch, Route } from 'react-router-dom'
+import Login from './components/Login.js'
+import Admin from './components/Admin'
+import Logout from './components/Logout'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      // <h1>Hello</h1>
+      <Switch>
+        <Route exact path="/" component={Login}></Route>
+        <Route path="/admin" component={Admin}></Route>
+        <Route path="/logout" component={Logout}></Route>
+
+      </Switch>      
+    );
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/Logout.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+export class Logout extends Component {
+    constructor(props) {
+        super(props)
+        localStorage.removeItem("token")    
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1> you have logged out</h1>
+                <Link to='/'>Login Again</Link>                
+            </div>
+        )
+    }
+}
+
+export default Logout
